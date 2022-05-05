8e3397f15d9541dd0da82431e2d2b7383d5af963
radcoblue <sylerblue@gmail.com>
2022-05-05 09:16:17 -0400

    yeah

---
+++ proyecto_1/animacion.js
@@ -0,0 +1,18 @@
+function ocultar(){
+
+    $('.jan,.j').hide(4000);
+
+}
+
+function ver(){
+    $('.jan,.j').show(3000);
+}
+
+
+
+$(document).ready(function(){
+
+    $('body').ripples({resolution:512,dropRadius:20,perturbance:0.04});
+});
+
+
